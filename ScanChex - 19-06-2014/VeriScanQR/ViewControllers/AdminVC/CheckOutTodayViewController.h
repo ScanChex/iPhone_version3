9725eb6717a2b1439b9b2f131d26eec46d2d4034
qsoftdevelopment <qsoftdevelopment@gmail.com>
2014-11-05 22:15:27 +0530

    latest updates

---
+++ ScanChex - 19-06-2014/VeriScanQR/ViewControllers/AdminVC/CheckOutTodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  CheckOutTodayViewController.h
+//  ScanChex
+//
+//  Created by Ravi Kumar Bandaru on 10/24/14.
+//  Copyright (c) 2014 Adnan Ahmad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckOutTodayViewController : UIViewController
+
+@end
